fad8c0ac5da2f918c452d7006100b4d210766c6c
Kylin <37763338+Kylinink@users.noreply.github.com>
2026-01-10 08:36:30 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,3 +2,7 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+rm -rf feeds/packages/net/open-app-filter
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
