e83155b0d70b1de718ddeadee5308084c8f094d8
Kylin <37763338+Kylinink@users.noreply.github.com>
2026-01-10 09:58:39 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -5,4 +5,4 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 
 rm -rf feeds/packages/net/open-app-filter
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
+# git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
